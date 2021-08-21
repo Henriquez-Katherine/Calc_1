b898cc28904ff4306aa3d9bf06a5e5a9d230e424
Henriquez-Katherine <89306792+Henriquez-Katherine@users.noreply.github.com>
2021-08-22 00:06:42 +0700

    Add files via upload

--- Calc.py
+++ Calc.py
@@ -1,50 +1,57 @@
+import time
 #Start
-Check = True 
-while Check:
-    #Load function for calculations
-    def Calculate(num1, num2, oper):
-        if oper == "+":
-            ans = num1 + num2
-            return ans
-        elif oper == "-":
-            ans = num1 - num2
-            return ans
-        elif oper == "*":
-            ans = num1 * num2
-            return ans
-        elif oper == "/":
+print ("\n" * 40)
+def main():
+        Check = True 
+        while Check:
+            #Load function for calculations
+            def Calculate(num1, num2, oper):
+                if oper == "+":
+                    ans = num1 + num2
+                    return ans
+                elif oper == "-":
+                    ans = num1 - num2
+                    return ans
+                elif oper == "*":
+                    ans = num1 * num2
+                    return ans
+                elif oper == "/":
+                    try:
+                        ans = num1 / num2
+                        return ans
+                    except:
+                        print ("Null Division Error!!")
+                        return 9999999999999999999999999999999999999999999999999999999999
+                else:
+                    print (Error)
+                    return 0
+            print ("Calc_v0.1")
+            print ("Write first number: ")
+            # Catch input
+            num1 = input("Enter: ")
+            print ("Write second number: ")
+            num2 = input("Enter: ")
+            # Check numbers
             try:
-                ans = num1 / num2
-                return ans
+                num1 = int(num1)
+                num2 = int(num2)
             except:
-                print ("Null Division Error!!")
-                return 9999999999999999999999999999999999999999999999999999999999
-        else:
-            print (Error)
-            return 0
-    print ("Calc_v0.1")
-    print ("Write first number: ")
-    # Catch input
-    num1 = input("Enter: ")
-    print ("Write second number: ")
-    num2 = input("Enter: ")
-    # Check numbers
-    try:
-        num1 = int(num1)
-        num2 = int(num2)
-    except:
-        print("Error, please try again!")
-        break
-    #Operation
-    Checking = True # Wait right name of operations
-    while Checking:
-        print ("Write operation: + - * /")
-        oper = input("Enter: ")
-        if oper == "-" or oper == "+" or oper == "*" or oper == "/": #
-            Checking = False         
-        else:
-            print ("Error, try again")      
-    answer = Calculate(num1, num2, oper)
-    print (f"Answer: {answer}")
-
-print ("Program closed due to Error or something other...")
+                print("Error, please try again!")
+                break
+            #Operation
+            Checking = True # Wait right name of operations
+            while Checking:
+                print ("Write operation: + - * /")
+                oper = input("Enter: ")
+                if oper == "-" or oper == "+" or oper == "*" or oper == "/": #
+                    Checking = False         
+                else:
+                    print ("Error, try again")      
+            answer = Calculate(num1, num2, oper)
+            print (f"Answer: {answer}")
+            time.sleep(4) # Wait, Show answer
+            print ("\n" * 40)
+        main() # If we catch error, We begin function again
+        
+main() # Start function
+print ("Program closed due to Error or something other... If you get that message rebort program.")
